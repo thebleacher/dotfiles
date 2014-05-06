4e2ca873fd255f8b47bb1a30603ae7834238f321
Martijn Bleeker <martijn@keplar.nl>
2014-05-06 16:02:19 +0200

    Alias housekeeping.

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -4,15 +4,14 @@ alias reload="source $HOME/.zshrc"
 alias lock="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
 alias sudo="nocorrect sudo"
 alias ..="cd .."
-alias o="open ."
 alias mou="open -a mou"
 alias la="ls -la"
-alias cr="chruby"
 alias desk="cd $HOME/Desktop"
 alias home="cd $HOME/"
 alias dev="cd $HOME/Development/"
 alias dotfiles="cd $HOME/Development/dev/dotfiles"
 alias kep="cd $HOME/Development/keplar/"
+alias mm="cd $HOME/Development/meukmap/"
 if [[ $(uname -s) == "Darwin" ]]; then
   alias flushdns="dscacheutil -flushcache"
 else
@@ -41,13 +40,18 @@ alias be="bundle exec"
 alias ber="be rake"
 alias bet="be rspec"
 alias cbet="COV=1 be rspec"
-alias fs="be foreman start -f Procfile.development"
-alias fsp="be foreman start"
-alias gu="be guard"
+
+#Bundle exec spring
+alias bs="bundle exec spring"
+alias bsr="bs rake"
+alias bst="bs rspec"
+alias cbst="COV=1 bs rspec"
 
 # Rake tasks
 alias rr="be rake routes"
 alias rake="noglob rake" # allows square brackts for rake task invocation
+
+alias srr="bs rake routes"
 
 # Middleman
 alias ms="be middleman s"
@@ -58,16 +62,21 @@ alias PROD="RAILS_ENV=production"
 alias TEST="RAILS_ENV=test"
 alias DEV="RAILS_ENV=development"
 
-alias rs="bundle exec rails server"
-alias rc="bundle exec rails console"
-alias rg="bundle exec rails generate"
+alias rs="be rails s"
+alias rc="be rails c"
+alias rg="be rails g"
 
-alias rdm="bundle exec rake db:migrate"
-alias rdr="bundle exec rake db:migrate:reset"
-alias rds="bundle exec rake db:seed"
+alias srs="bs rails s"
+alias src="bs rails c"
+alias srg="bs rails g"
+
+alias rdm="be rake db:migrate"
+alias rdr="be rake db:migrate:reset"
+alias rds="be rake db:seed"
+
+alias srdm="bs rake db:migrate"
+alias srdr="bs rake db:migrate:reset"
+alias srds="bs rake db:seed"
+
 alias rdi="rdr && rds"
 alias rdsd="rdr && SEED_DEMO=1 bundle exec rake db:seed"
-
-# Oh openssl on Mac..... There are no words
-alias sslhack="echo \"OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE\" > config/initializers/opensslhack.rb"
-alias sslremove="rm config/initializers/opensslhack.rb"
