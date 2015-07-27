429a205d50a2007eaa1f9f908f249e7e86ace32a
Martijn Bleeker <martijn@keplar.nl>
2015-07-27 13:15:52 +0200

    Rough hammerspoon setup

---
+++ hammerspoon/init.lua
@@ -0,0 +1,10 @@
+local application = require "hs.application"
+local hotkey = require "hs.hotkey"
+local alert = require "hs.alert"
+
+require "triggers"
+require "window_management"
+-- require "auto_muter"
+-- require "application_watcher"
+
+alert.show("Hammerspoon loaded!")
---
+++ hammerspoon/window_management.lua
@@ -0,0 +1,98 @@
+local hotkey = require "hs.hotkey"
+local window = require "hs.window"
+local screen = require "hs.screen"
+local alert = require "hs.alert"
+local keycodes = require "hs.keycodes"
+
+window.animationDuration = 0
+
+-- Make window full screen
+hotkey.bind({"ctrl", "alt"}, "up", function()
+  local win = window.focusedWindow()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  win:setFrame(max)
+end)
+
+-- Align window to center of the screen
+hotkey.bind({"ctrl", "alt"}, "down", function()
+  local win = window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = (max.w - f.w) / 2
+  f.y = (max.h - f.h) / 2
+  f.w = 1920
+  f.h = 1080
+  win:setFrame(f)
+end)
+
+-- Align window to left 50% of the screen
+hotkey.bind({"ctrl", "alt"}, "left", function()
+  local win = window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- Align window to right 50% of the screen
+hotkey.bind({"ctrl", "alt"}, "right", function()
+  local win = window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+-- -- Align window to top left of the screen
+-- hotkey.bind({"cmd", "ctrl"}, "[", function()
+--   local win = window.focusedWindow()
+--   local f = win:frame()
+--   local screen = win:screen()
+--   local max = screen:frame()
+--
+--   f.x = 0
+--   f.y = 0
+--   f.w = max.w / 2
+--   f.h = max.h / 2
+--   win:setFrame(f)
+-- end)
+--
+-- -- Align window to top right of the screen
+-- hotkey.bind({"cmd", "ctrl"}, "]", function()
+--   local win = window.focusedWindow()
+--   local f = win:frame()
+--   local screen = win:screen()
+--   local max = screen:frame()
+--
+--   f.x = max.w / 2
+--   f.y = 0
+--   f.w = max.w / 2
+--   f.h = max.h / 2
+--   win:setFrame(f)
+-- end)
+--
+-- Move window to the display on the left
+hotkey.bind({"cmd", "ctrl", "alt"}, "left", function()
+  local win = hs.window.focusedWindow()
+  win:moveOneScreenWest()
+end)
+
+-- Move window to the display on the right
+hotkey.bind({"cmd", "ctrl", "alt"}, "right", function()
+  local win = hs.window.focusedWindow()
+  win:moveOneScreenEast()
+end)
