6f4d711b5bc750337d9c980ac419927686669738
Martijn Bleeker <martijn@keplar.nl>
2015-10-30 17:18:23 +0100

    Add 415 alias

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -12,6 +12,7 @@ alias home="cd $HOME/"
 alias dev="cd $HOME/Development/"
 alias dotfiles="cd $HOME/Development/dev/dotfiles"
 alias kep="cd $HOME/Development/keplar/"
+alias 4="cd $HOME/Development/415/"
 alias exp="cd $HOME/Development/experiments/"
 alias mm="cd $HOME/Development/meukmap/"
 if [[ $(uname -s) == "Darwin" ]]; then
