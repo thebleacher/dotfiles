e10053a63aef449368308d384ee5a1ffdd39cdfd
Martijn Bleeker <martijn@thebleacher.com>
2016-01-28 17:19:47 +0100

    Remove spring aliases

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -44,17 +44,9 @@ alias ber="be rake"
 alias bet="be rspec"
 alias cbet="COV=1 be rspec"
 
-#Bundle exec spring
-alias s="spring"
-alias sr="s rake"
-alias st="s rspec"
-alias cbst="COV=1 s rspec"
-
 # Rake tasks
 alias rr="be rake routes"
 alias rake="noglob rake" # allows square brackts for rake task invocation
-
-alias srr="s rake routes"
 
 # Middleman
 alias ms="be middleman s"
@@ -69,17 +61,9 @@ alias rs="be rails s"
 alias rc="be rails c"
 alias rg="be rails g"
 
-alias srs="s rails s"
-alias src="s rails c"
-alias srg="s rails g"
-
 alias rdm="be rake db:migrate"
 alias rdr="be rake db:migrate:reset"
 alias rds="be rake db:seed"
-
-alias srdm="s rake db:migrate"
-alias srdr="s rake db:migrate:reset"
-alias srds="s rake db:seed"
 
 alias rdi="rdr && rds"
 alias rdsd="rdr && SEED_DEMO=1 bundle exec rake db:seed"
