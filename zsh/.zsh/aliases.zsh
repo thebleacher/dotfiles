149580a61a49d85d86a50e5d034cbf33d8e8be73
Martijn Bleeker <martijn@thebleacher.com>
2016-03-25 11:35:26 +0100

    Set default binding for rails server

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -57,7 +57,7 @@ alias PROD="RAILS_ENV=production"
 alias TEST="RAILS_ENV=test"
 alias DEV="RAILS_ENV=development"
 
-alias rs="be rails s"
+alias rs="be rails s --binding=0.0.0.0"
 alias rc="be rails c"
 alias rg="be rails g"
 
